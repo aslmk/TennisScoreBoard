dbade54d136c0ae89b0e3ee5914f19a96c7344bc
aslmk <aulaensov@gmail.com>
2024-10-27 16:48:56 +0500

    Refactor PlayersDAO class

--- src/main/java/aslmk/DAO/PlayersDAO.java
+++ src/main/java/aslmk/DAO/PlayersDAO.java
@@ -9,51 +9,28 @@ import org.hibernate.query.Query;
 
 public class PlayersDAO {
     public Player getPlayerByName(String name) {
-        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        Transaction transaction = null;
-        Player player = null;
-
-        try {
-            transaction = session.beginTransaction();
-
+        Player player;
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
             String hql = "FROM Player WHERE name = :name";
             Query<Player> query = session.createQuery(hql, Player.class);
             query.setParameter("name", name);
             player = query.uniqueResult();
             transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            session.close();
         }
 
         return player;
     }
     public Player getPlayerById(int id) {
-        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        Transaction transaction = null;
-        Player player = null;
-
-        try {
-            transaction = session.beginTransaction();
-
+        Player player;
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
             String hql = "FROM Player WHERE id = :id";
             Query<Player> query = session.createQuery(hql, Player.class);
             query.setParameter("id", id);
             player = query.uniqueResult();
             transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            session.close();
         }
-
         return player;
     }
     public void createPlayer(Player player) throws PlayerSaveFailedException {
